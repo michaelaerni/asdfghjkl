Fixed batch gradient calculation

asdfghjkl/gradient.py
@@ -61,21 +61,25 @@ def data_loader_gradient(
 
 def batch_gradient(model, loss_fn, inputs, targets):
     n = len(inputs)
-    with extend(model, OP_BATCH_GRADS):
+    with extend(model, OP_BATCH_GRADS) as ctx:
         model.zero_grad()
         f = model(inputs)
         loss = loss_fn(f, targets)
         loss.backward()
-        batch_grad_list = _get_batch_grad_list(model)
+        batch_grad_list = _get_batch_grad_list(model, ctx)
         grads = torch.cat([g.view(n, -1) for g in batch_grad_list], dim=1)  # (n, p)
     return grads, f
 
     
-def _get_batch_grad_list(model):
+def _get_batch_grad_list(model, ctx):
     batch_grad_list = list()
     for module in model.modules():
-        if hasattr(module, 'operation'):
-            res = module.operation._op_results['batch_grads']
+        if ctx.is_operation_registered(module):
+            operation = ctx.get_operation(module)
+            if OP_BATCH_GRADS not in operation._op_names:
+                continue
+
+            res = operation._op_results[OP_BATCH_GRADS]
             if 'weight' in res:
                 batch_grad_list.append(res['weight'])
             if 'bias' in res:
@@ -86,6 +90,7 @@ def _get_batch_grad_list(model):
 
 
 def jacobian(model, x):
+    # TODO: This method most likely also does not work, but could not find any usages
     f = model(x)
     assert f.ndim == 2  # (n, c)
     n, c = f.shape